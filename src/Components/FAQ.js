25516e33a43a20a29787a1adae63ff654199da70
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-20 19:45:35 -0300

    Fix: FAQ error responsive

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import '../css/FAQ.css';
 
 const FAQ = ({ toggleFAQ }) => {
     return (
