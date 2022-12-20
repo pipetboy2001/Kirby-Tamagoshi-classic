3f3cd26a2dbd4a3c06a2b6ef365251fd774642d9
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-20 17:34:25 -0300

    feature: Icons and Fonts

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -4,10 +4,13 @@ import { ProgressBar } from 'react-bootstrap';
 import { Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/Game.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faCaretSquareRight, faLeaf } from '@fortawesome/free-solid-svg-icons';
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+//icons
+import { GrGamepad } from 'react-icons/gr';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { MdOutlineHealthAndSafety } from 'react-icons/md';
+import { BiTime } from 'react-icons/bi';
+import { GiLaurelsTrophy } from 'react-icons/gi';
+import { MdOutlineAdsClick } from 'react-icons/md';
 
 export const Game = () => {
     // Estados para los parámetros de Tamagoshi
@@ -18,8 +21,6 @@ export const Game = () => {
     const [record, setRecord] = useState(0);
     const [showModal, setShowModal] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
-
-
     // Hook para ejecutar un intervalo
     let tamagoshiImage;
     //si hambre es menor o igual a 30 o felicidad es menor o igual a 30 o salud es menor o igual a 30
@@ -70,13 +71,10 @@ export const Game = () => {
             });
         }
     }, 30000);
-
     // Función para actualizar el tiempo de juego
     useInterval(() => {
         setElapsedTime(elapsedTime + 1);
     }, 1000); // Actualiza cada segundo
-
-
     // Función para actualizar el estado de hambre del Tamagoshi
     useInterval(() => {
         setHunger(hunger - 1);
@@ -122,7 +120,6 @@ export const Game = () => {
                     element: 'div',
                     attributes: {
                         className: 'custom-swal-text, custom-swal'
-
                     }
                 }
             }).then((result) => {
@@ -136,24 +133,6 @@ export const Game = () => {
             });
         }
     }, 3000);
-
-    // Función para actualizar el estado de hambre del Tamagoshi
-    const feed = () => {
-        setHunger(hunger + 10 > 100 ? 100 : hunger + 10);
-    };
-
-    // Función para actualizar el estado de felicidad del Tamagoshi
-    const play = () => {
-        setHappiness(happiness + 10 > 100 ? 100 : happiness + 10);
-    };
-
-    // Función para actualizar el estado de salud del Tamagoshi
-    const heal = () => {
-        setHealth(health + 5 > 100 ? 100 : health + 5);
-    };
-
-
-
     // Función para actualizar el record de edad
     const updateRecord = () => {
         if (age > record) {
@@ -161,13 +140,11 @@ export const Game = () => {
             localStorage.setItem('record', age);
         }
     }
-
     // Formatea el tiempo de juego
     const minutes = Math.floor(elapsedTime / 60);
     const seconds = elapsedTime % 60;
     // Agrega un 0 a la izquierda si el número es menor a 10
     const elapsedTimeFormatted = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-
     // Actualiza el record de edad cuando cambia la edad
     useEffect(updateRecord, [age, record]);
     // Carga el record de edad desde el localStorage
@@ -182,26 +159,24 @@ export const Game = () => {
     return (
         <div className="egg">
             <h1 className="character-name">Kirby</h1>
-            <h2 className="character-age"><FontAwesomeIcon icon={faClock} /> Edad: {age} Años</h2>
-            <h2 className="character-age"><FontAwesomeIcon icon={faTrophy} /> Record: {record} Años</h2>
-            <button onClick={() => setShowModal(true)}>Estado</button>
+            <h2 className="character-age"> <BiTime /> Edad: {age} Años</h2>
+            <h2 className="character-age"> <GiLaurelsTrophy /> Record: {record} Años</h2>
+            <button className='buttonsUp' onClick={() => setShowModal(true)}><MdOutlineAdsClick /> Estado</button>
             <div className="square">
                 <img src={tamagoshiImage} alt="Imagen del cuadrado" />
             </div>
             <div className="buttons">
                 <button onClick={() => setHunger(Math.min(hunger + 10, 100))}>
-                    <FontAwesomeIcon icon={faLeaf} /> Comer
+                    <IoFastFoodOutline /> Comer
                 </button>
                 <button onClick={() => setHappiness(Math.min(happiness + 10, 100))}>
-                    <FontAwesomeIcon icon={faHeart} /> Jugar
+                    <GrGamepad /> Jugar
                 </button>
                 <button onClick={() => setHealth(Math.min(health + 10, 100))}>
-                    <FontAwesomeIcon icon={faCaretSquareRight} /> Sanar
+                    <MdOutlineHealthAndSafety /> Sanar
                 </button>
-
             </div>
             <h1 className="character-name">Tiempo de juego: {elapsedTimeFormatted} Seg.</h1>
-
             <Modal show={showModal} onHide={() => setShowModal(false)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Estado del Kirby</Modal.Title>
@@ -212,7 +187,7 @@ export const Game = () => {
                     Hambre: <ProgressBar now={hunger} variant="warning" label={`${hunger}%`} />
                     Felicidad: <ProgressBar now={happiness} variant="success" label={`${happiness}%`} />
                     Salud: <ProgressBar now={health} variant="info" label={`${health}%`} />
-                    
+
                 </Modal.Body>
 
             </Modal>
